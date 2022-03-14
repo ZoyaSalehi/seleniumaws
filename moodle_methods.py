dd85d1805a406b8e21f5592119da29a22868c236
Zoya <zoya@clearviewsys.com>
2022-03-14 10:57:41 -0700

    Added selenium scripts

--- moodle_methods.py
+++ moodle_methods.py
@@ -8,8 +8,24 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select  # <--- add this import for drop down lists
 from selenium.webdriver import Keys
 
-s = Service(executable_path='../chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
+
+
+# s = Service(executable_path='../chromedriver.exe')
+# driver = webdriver.Chrome(service=s)
 
 
 def setUp():
